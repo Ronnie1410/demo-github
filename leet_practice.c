d65baa0d706efec4de4d2f65616aa38eb774a40b
Ronnie1410 <Ronnie14_10@protonmail.com>
2022-07-28 14:37:07 -0700

    Initial Commit for .gitignore. Initial commit leet_practice.c

---
+++ leet_practice.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIN(a,b)  ((a)<(b))?(a):(b)
+
+int main()
+{
+    int a=5, b=8, min;
+    min =  MIN(a,b);
+    printf("min is %d \n", min);
+    int *p = &a;
+    min = MIN(*p,b);
+    #ifndef HEADER
+        printf("not defined\n");
+    #else
+        printf("defined\n");
+    #endif
+    printf("min is %d\n",min);
+    for (int i =0; i<10; i++)
+    {
+        if(i % 2 == 0)
+        {
+            printf("%d \n", i);
+        }
+    }
+    return 0;
+}
